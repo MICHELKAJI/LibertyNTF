283f0bee1a75b4e3753a1c928cea2f38ca244f75
Michel <fistonkajibwami2@gmail.com>
2024-06-13 03:46:00 -0330

    add section kind of browse

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { BannerHomeComponent } from '../banner-home/banner-home.component';
+import { KindofBrowseComponent } from '../kindof-browse/kindof-browse.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
-    BannerHomeComponent
+    BannerHomeComponent,
+    KindofBrowseComponent
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
